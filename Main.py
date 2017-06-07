19b6bf614f756d4fcf294dbc692536aa2b761b35
Gary Muller <gary@mullerpoint.com>
2017-06-07 14:42:48 -0500

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+##########################################################
+#
+# Summer 2017 Project 1
+# Gary Muller
+# Dr Rohit Valecha
+# University of Texas at San Antonio
+# A project to classify text as specific or generic
+#
+##########################################################
+
+##########################################################
+# Imports
+##########################################################
+
+import numpy as np              # Used For Statistics Functions
+import pandas as pd             # Used to store data
+
+##########################################################
+# Main
+##########################################################
+def main():
+    xls_file = pd.ExcelFile()
+
+
+
+if __name__ == '__main__':
+    main()
