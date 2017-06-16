50e4b2285ff8acdacbc1b2f04ba840e392aa4156
Gary Muller <gary@mullerpoint.com>
2017-06-16 14:24:29 -0500

    complete change to use Named Entity Recognition to identify locations

--- Main.py
+++ Main.py
@@ -15,10 +15,10 @@
 # Imports
 ##########################################################
 
-import numpy as np                          # Used For Statistics Functions
-import pandas as pd                         # Used to store data
-from nltk.tag import pos_tag                # Used to tag words with their part of speech
-from nltk.tokenize import word_tokenize     # Used to tokenize text down to words
+import numpy as np          # Used For Statistics Functions
+import pandas as pd         # Used to store data
+import spacy                # Used to do named entity recognition
+
 
 
 ##########################################################
@@ -31,47 +31,30 @@ def main():
     xls_file = pd.ExcelFile("./Context_Extraction.xlsx")
     df = xls_file.parse('Final')
 
-    print(df.text_heading)  # DEBUG
+    print(df['text_heading'][0])  # DEBUG
 
-    # Tokenize the text and save the list of tokens as a new column
-    token_list = []
+    nlp = spacy.load('en')
+    # doc = nlp('this is a test sentence typed in San Antonio by Gary')
+
+
+    ner_list = []
+    temp_int = 0  # DEBUG
     for row in df['text_heading']:
-        token_list.append(tokenize(row))
-    df['tokens'] = token_list
+        tagged_str = nlp(row)
+        ner_list.append(tagged_str)
+        temp_int = temp_int + 1  # DEBUG
+        print(temp_int)  # DEBUG
 
-    print(df['tokens'])  # DEBUG
+    df['ner_tags'] = ner_list
 
-    # Get the number of tokens in each row and save the number as a new column
-    len_list = []
-    for row in df['tokens']:
-        len_list.append(len(row))
-    df['text_length'] = len_list
 
-    # Count verbs
-    # df['verb_count'] = count_verbs(df['tokens'])
 
-    # do clustering analysis
-    # TODO
 
-    print(df['text_length'])  # DEBUG
-
-    # Make determination of Generic/Specific based on clusters and save as a column
-    # TODO
 
 
 ##########################################################
 # Functions
 ##########################################################
-
-
-def tokenize(in_string):
-    string_tagged = pos_tag(word_tokenize(in_string))
-    return string_tagged
-
-
-def count_verbs():
-    # TODO
-    return -1
 
 
 if __name__ == '__main__':
